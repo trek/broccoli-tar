b06492faba11d3df209d6a62c754d3520cb5ff0f
Trek Glowacki <trek.glowacki@gmail.com>
2014-11-23 08:54:30 -0600

    WIP?

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-var targz = require('tar.gz');
+var tar = require("tar");
+var fstream = require("fstream")
+var fs = require("fs");
+
 var RSVP = require('rsvp');
 var quickTemp = require('quick-temp');
 
@@ -12,17 +15,22 @@ TarGzip.prototype = {
   read: function(readTree){
     var destDir = quickTemp.makeOrRemake(this, 'tmpDestDir');
     var outName = this.options.name;
+    var tarName = [destDir, '/', outName, '.tar'].join('');
+
+    var tarFile = fs.createWriteStream(tarName)
 
     return readTree(this.inputTree).then(function (srcDir){
-      return new RSVP.Promise(function(resolve, reject) {
-        var outfile = [destDir, '/', outName, '.tar.gz'].join('');
-        new targz().compress(srcDir, outfile, function(err){
-          if (err) {
-            reject(err);
-          } else {
-            resolve(destDir);
-          }
-        });
+      return new RSVP.Promise(function(resolve, reject){
+
+        var packer = tar.Pack({ noProprietary: true })
+          .on('error', reject)
+          .on('end', function(){ resolve(destDir); });
+
+        // This must be a "directory"
+        fstream.Reader({ path: srcDir, type: "Directory", follow: true})
+          .on('error', reject)
+          .pipe(packer)
+          .pipe(tarFile);
       });
     });
   },
