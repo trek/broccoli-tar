fb6ed98f0c9c5afa4f05fb35ea1a74df50356e49
Trek Glowacki <trek@groupon.com>
2015-01-23 11:04:53 -0600

    Shell out to tar

--- index.js
+++ index.js
@@ -1,45 +1,22 @@
-var fs = require("fs");
 var RSVP = require('rsvp');
 var quickTemp = require('quick-temp');
-var walkSync = require('walk-sync');
-var Tar = require('tar-async/tar');
+var exec = RSVP.denodeify(require('child_process').exec);
 
-TarGzip = function TarGzip(inputTree, options){
+TarGzip = function TarGzip(inputTree, name){
   this.inputTree = inputTree;
-  this.options = options || {};
-  this.options.name = this.options.name || 'archive';
-}
-
-function onError(e){
-  console.warn(e);
+  this.name = name || 'archive';
 }
 
 TarGzip.prototype = {
   read: function(readTree){
     var destDir = quickTemp.makeOrRemake(this, 'tmpDestDir');
-    var outName = this.options.name;
-    var tarName = [destDir, '/', outName, '.tar'].join('');
+
+    var outName = this.name;
+    var tarName = [destDir, '/', outName, '.tar.gz'].join('');
 
     return readTree(this.inputTree).then(function (srcDir){
-      var paths = walkSync(srcDir);
-      var tape = new Tar({output: fs.createWriteStream(tarName)});
-
-      var writes = paths.map(function(p){
-        var path = srcDir + "/" + p;
-
-        if (fs.lstatSync(path).isDirectory()) {
-          return new RSVP.Promise(function(resolve, reject){ resolve(true)});
-        }
-
-        return new RSVP.Promise(function(resolve, reject){
-          var f = fs.createReadStream(srcDir + "/" + p).on('error', reject);
-          tape.append(p, f, {allowPipe: true}, resolve);
-        });
-      });
-
-
-      return RSVP.all(writes).then(function(){
-        console.log("????");
+      var args = ['tar', 'chz', '-C', srcDir, '-f', tarName, '.'].join(' ');
+      return exec(args).then(function(){
         return destDir;
       });
     });
