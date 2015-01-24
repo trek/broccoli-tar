0a828212c37f44e25e0e7a0ae1911c38fe9c8dd8
Jason Williams <jaswilli@gmail.com>
2015-01-23 23:08:07 -0600

    Use node-tar instead of exec
    Closes #2
    - Swap out cp.exec to system tar for node-tar module.
    - Add body of test that checks content of tar file.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var RSVP = require('rsvp');
 var quickTemp = require('quick-temp');
-var exec = RSVP.denodeify(require('child_process').exec);
+var fs = require('fs');
+var fstream = require('fstream');
+var tar = require('tar');
+var zlib = require('zlib');
 
-TarGzip = function TarGzip(inputTree, name){
+var TarGzip = function TarGzip(inputTree, name){
   this.inputTree = inputTree;
   this.name = name || 'archive';
-}
+};
 
 TarGzip.prototype = {
   read: function(readTree){
@@ -15,9 +18,20 @@ TarGzip.prototype = {
     var tarName = [destDir, '/', outName, '.tar.gz'].join('');
 
     return readTree(this.inputTree).then(function (srcDir){
-      var args = ['tar', 'chz', '-C', srcDir, '-f', tarName, '.'].join(' ');
-      return exec(args).then(function(){
-        return destDir;
+      return new RSVP.Promise(function (resolve, reject) {
+        var reader = fstream.Reader({path: srcDir, follow: true});
+        var writer = fs.createWriteStream(tarName);
+        var packer = tar.Pack();
+        var gz = zlib.createGzip();
+
+        reader.on('error', reject);
+        gz.on('error', reject);
+        writer.on('error', reject);
+        writer.on('finish', function (){
+          resolve(destDir);
+        });
+
+        reader.pipe(packer).pipe(gz).pipe(writer);
       });
     });
   },
